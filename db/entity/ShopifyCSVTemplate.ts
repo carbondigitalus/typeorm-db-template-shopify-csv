077078ca3adc8ca3881e2cb2f9f01d01b037fb3f
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-03-26 01:58:14 -0400

    Create DB Entity with columns

--- db/entity/ShopifyCSVTemplate.ts
+++ db/entity/ShopifyCSVTemplate.ts
@@ -34,3 +34,253 @@ import {
     VariantInventoryTracker,
     VariantWeightUnit
 } from '../../enums';
+
+@Entity()
+export default class ShopifyCSVTemplate {
+    // COLUMNS
+    @PrimaryGeneratedColumn('uuid')
+    @IsNotEmpty()
+    id: number;
+
+    @Column({ name: 'Handle' })
+    @IsString()
+    @IsNotEmpty()
+    handle: string;
+
+    @Column({ name: 'Title' })
+    @IsString()
+    title?: string;
+
+    @Column({ name: 'Body (HTML)' })
+    @IsString()
+    body?: string;
+
+    @Column({ name: 'Vendor' })
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(2, {
+        message: 'Min of 2 characters required. Values can be blank spaces.'
+    })
+    vendor: string;
+
+    @Column({ name: 'Standardized Product Type' })
+    @IsString()
+    standardizedProductType?: string;
+
+    @Column({ name: 'Custom Product Type' })
+    @IsString()
+    customProductType?: string;
+
+    @Column({ name: 'Tags' })
+    @IsString()
+    tags?: string;
+
+    @Column({ name: 'Published', default: true })
+    @IsBoolean()
+    @IsNotEmpty()
+    published: boolean;
+
+    @Column({ name: 'Option1 Name' })
+    @IsString()
+    @IsNotEmpty()
+    option1Name: string;
+
+    @Column({ name: 'Option1 Value' })
+    @IsString()
+    @IsNotEmpty()
+    option1Value: string;
+
+    @Column({ name: 'Option2 Name' })
+    @IsString()
+    option2Name?: string;
+
+    @Column({ name: 'Option2 Value' })
+    @IsString()
+    option2Value?: string;
+
+    @Column({ name: 'Option3 Name' })
+    @IsString()
+    option3Name?: string;
+
+    @Column({ name: 'Option3 Value' })
+    @IsString()
+    option3Value?: string;
+
+    @Column({ name: 'Variant SKU' })
+    @IsString()
+    variantSKU?: string;
+
+    @Column({ name: 'Variant Grams' })
+    @IsNumber()
+    @IsNotEmpty()
+    @Min(0, { message: 'Weight must be at least 0 or greater.' })
+    variantGrams: number;
+
+    @Column({ name: 'Variant Inventory Tracker' })
+    @IsString()
+    variantInventoryTracker?: VariantInventoryTracker;
+
+    @Column({ name: 'Variant Inventory Qty' })
+    @IsNumber()
+    @IsInt()
+    variantInventoryQty?: number;
+
+    @Column({ name: 'Variant Inventory Policy' })
+    @IsString()
+    @IsNotEmpty()
+    variantInventoryPolicy: VariantInventoryPolicy;
+
+    @Column({ name: 'Variant Fulfillment Service' })
+    @IsString()
+    variantFulfillmentService: VariantFulfillmentService;
+
+    @Column({ name: 'Variant Price' })
+    @IsNotEmpty()
+    @Matches(PriceCheck.prototype.Decimal || PriceCheck.prototype.Whole, {
+        message:
+            'A price must be 12 digits or less. The last 2 digits come after the decimal ($.00) to represent the cents.'
+    })
+    variantPrice: PriceCheck;
+
+    @Column({ name: 'Variant Compare At Price' })
+    @Matches(PriceCheck.prototype.Decimal || PriceCheck.prototype.Whole, {
+        message:
+            'A price must be 12 digits or less. The last 2 digits come after the decimal ($.00) to represent the cents.'
+    })
+    variantCompareAtPrice?: PriceCheck;
+
+    @Column({ name: 'Variant Requires Shipping', default: true })
+    @IsBoolean()
+    @IsNotEmpty()
+    variantRequiresShipping: boolean;
+
+    @Column({ name: 'Variant Taxable', default: true })
+    @IsBoolean()
+    @IsNotEmpty()
+    variantTaxable: boolean;
+
+    @Column({ name: 'Variant Barcode' })
+    @IsString()
+    variantBarcode: string;
+
+    @Column({ name: 'Image Src' })
+    @IsString()
+    @IsUrl()
+    @NotContains('_thumb')
+    @NotContains('_small')
+    @NotContains('_medium')
+    imageSrc: string;
+
+    @Column({ name: 'Image Position' })
+    @IsNumber()
+    imagePosition?: number;
+
+    @Column({ name: 'Image Alt Text' })
+    @IsString()
+    @MaxLength(512, {
+        message: 'Max length for an alt text is 512 characters.'
+    })
+    imageAltText?: string;
+
+    @Column({ name: 'Gift Card', default: false })
+    @IsBoolean()
+    @IsNotEmpty()
+    giftCard: boolean;
+
+    @Column({ name: 'SEO Title' })
+    @IsString()
+    @MaxLength(70, { message: 'Max length for an SEO title is 70 characters.' })
+    seoTitle?: string;
+
+    @Column({ name: 'SEO Description' })
+    @IsString()
+    @MaxLength(320, {
+        message: 'Max length for an SEO description is 320 characters.'
+    })
+    seoDescription?: string;
+
+    @Column({ name: 'Google Shopping / Google Product Category' })
+    @IsString()
+    googleShoppingProductCategory?: string;
+
+    @Column({ name: 'Google Shopping / Gender' })
+    @IsString()
+    googleShoppingGender?: GoogleShoppingGender;
+
+    @Column({ name: 'Google Shopping / Age Group' })
+    @IsString()
+    googleShoppingAgeGroup?: GoogleShoppingAgeGroup;
+
+    @Column({ name: 'Google Shopping / MPN' })
+    @IsString()
+    @MinLength(1)
+    @MaxLength(70)
+    googleShoppingMPN?: string;
+
+    @Column({ name: 'Google Shopping / AdWords Grouping' })
+    @IsString()
+    googleShoppingAdWordsGrouping?: string;
+
+    @Column({ name: 'Google Shopping / AdWords Labels' })
+    @IsString()
+    googleShoppingAdWordsLabels?: string;
+
+    @Column({ name: 'Google Shopping / Condition' })
+    @IsString()
+    googleShoppingCondition?: GoogleShoppingCondition;
+
+    @Column({ name: 'Google Shopping / Custom Product' })
+    @IsString()
+    googleShoppingCustomProduct?: string;
+
+    @Column({ name: 'Google Shopping / Custom Label 0' })
+    @IsString()
+    googleShoppingCustomLabel0?: string;
+
+    @Column({ name: 'Google Shopping / Custom Label 1' })
+    @IsString()
+    googleShoppingCustomLabel1?: string;
+
+    @Column({ name: 'Google Shopping / Custom Label 2' })
+    @IsString()
+    googleShoppingCustomLabel2?: string;
+
+    @Column({ name: 'Google Shopping / Custom Label 3' })
+    @IsString()
+    googleShoppingCustomLabel3?: string;
+
+    @Column({ name: 'Google Shopping / Custom Label 4' })
+    @IsString()
+    googleShoppingCustomLabel4?: string;
+
+    @Column({ name: 'Variant Image' })
+    @IsString()
+    @IsUrl()
+    variantImage?: string;
+
+    @Column({ name: 'Variant Weight Unit' })
+    @IsString()
+    variantWeightUnit?: VariantWeightUnit;
+
+    @Column({ name: 'Variant Tax Code' }) // shopify plus clients need to research
+    @IsString()
+    @IsEmpty()
+    variantTaxCode?: string;
+
+    @Column({ name: 'Cost Per Item' })
+    @IsNumber()
+    costPerItem?: PriceCheck;
+
+    @Column({ name: 'Price / International' })
+    @IsNumber()
+    priceInternational?: PriceCheck;
+
+    @Column({ name: 'Compare At Price / International' })
+    @IsString()
+    compareAtPriceInternational: string;
+
+    @Column({ name: 'Status', default: ProductStatus.prototype.Active })
+    @IsString()
+    @IsNotEmpty()
+    status: ProductStatus;
+}
