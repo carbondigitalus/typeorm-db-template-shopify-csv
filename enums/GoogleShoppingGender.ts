c5a94d76d8bba1507e854cefafad2cda2752eeac
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-03-26 01:56:24 -0400

    Create GoogleShoppingGender.ts

---
+++ enums/GoogleShoppingGender.ts
@@ -0,0 +1,10 @@
+export class GoogleShoppingGender {
+    Male: string;
+    Female: string;
+    Unisex: string;
+    constructor() {
+        this.Male = 'male';
+        this.Female = 'female';
+        this.Unisex = 'unisex';
+    }
+}
