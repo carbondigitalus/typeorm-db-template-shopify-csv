92c422fc4085086214b12cd32391d84aa834ac06
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-03-26 01:56:27 -0400

    Create GoogleShoppingCondition.ts

---
+++ enums/GoogleShoppingCondition.ts
@@ -0,0 +1,12 @@
+export class GoogleShoppingCondition {
+    New: string;
+    Refurbished: string;
+    Used: string;
+    Damaged: string;
+    constructor() {
+        this.New = 'new';
+        this.Used = 'used';
+        this.Refurbished = 'refurbished';
+        this.Damaged = 'used';
+    }
+}
