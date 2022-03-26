e9add1349f1a7a901e5c706dbb3971603a58f83e
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-03-26 01:56:07 -0400

    Create VariantWeightUnit.ts

---
+++ enums/VariantWeightUnit.ts
@@ -0,0 +1,12 @@
+export class VariantWeightUnit {
+    Gram: string;
+    KiloGram: string;
+    Pound: string;
+    Ounce: string;
+    constructor() {
+        this.Gram = 'g';
+        this.KiloGram = 'kg';
+        this.Pound = 'lb';
+        this.Ounce = 'oz';
+    }
+}
