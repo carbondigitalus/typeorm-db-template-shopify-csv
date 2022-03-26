dd7a084182b7d3ddf535b2752c9158fc41858035
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-03-26 01:56:18 -0400

    Create ProductStatus.ts

---
+++ enums/ProductStatus.ts
@@ -0,0 +1,10 @@
+export class ProductStatus {
+    Active: string;
+    Draft: string;
+    Archived: string;
+    constructor() {
+        this.Active = 'active';
+        this.Draft = 'draft';
+        this.Archived = 'archived';
+    }
+}
