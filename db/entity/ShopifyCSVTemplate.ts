618ee9f53060ddab0c3d2aed398a14fbfbce24ac
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-03-26 01:57:14 -0400

    Add Shopify Required Fields ref

--- db/entity/ShopifyCSVTemplate.ts
+++ db/entity/ShopifyCSVTemplate.ts
@@ -1,2 +1,6 @@
 // Shopify CSV Template Ref:
 // https://help.shopify.com/en/manual/products/import-export/import-products
+
+// Required Field Ref:
+// https://firebearstudio.com/blog/the-ultimate-guide-to-shopify-import.html
+
